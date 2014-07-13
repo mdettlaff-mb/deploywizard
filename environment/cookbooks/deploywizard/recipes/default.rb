6c921778cba7fbe61b91657a07d1623f6a1cb810
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2014-07-13 12:06:48 +0200

    wip

--- environment/cookbooks/deploywizard/recipes/default.rb
+++ environment/cookbooks/deploywizard/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+microservice 'dw-eexample1' do
+  name 'dw-example1'
+end
+
+template "/opt/microservices/dw-example1/dw-example1.yml" do
+  source 'dw-example1.yml.erb'
+  owner node['deploywizard']['user']
+end
